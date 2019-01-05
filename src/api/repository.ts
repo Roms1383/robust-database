2fdcf77070efa0332ff6c03fca8630a4a8019149
Romain KELIFA <romain.kelifa@gmail.com>
2019-01-05 12:51:57 +0700

    :label: Add types to repository

--- src/api/repository.ts
+++ src/api/repository.ts
@@ -6,7 +6,7 @@ export class Repository {
   constructor(model : Model<Document>) {
     this.model = model
   }
-  public find = async (req, reply) => {
+  public find = async (req, reply) : Promise<Object[]|null|Error> => {
     try {
       const { id = undefined } = req.params
       return id
@@ -16,28 +16,29 @@ export class Repository {
       throw boom.boomify(e)
     }
   }
-  public create = async (req, reply) => {
+  public create = async (req, reply) : Promise<Object[]|Error> => {
     try {
       const { body: document } = req
-      return this.model.insertMany(document)
+      const inserted = await this.model.insertMany([document])
+      return inserted.map(document => document.toObject())
     } catch (e) {
       throw boom.boomify(e)
     }
   }
-  public update = async (req, reply) => {
+  public update = async (req, reply) : Promise<Object|Error> => {
     try {
       const { id } = req.params
       const { body: document } = req
       await this.model.findByIdAndUpdate(id, document)
-      return this.model.findById(id)
+      return this.model.findById(id).lean(true)
     } catch (e) {
       throw boom.boomify(e)
     }
   }
-  public delete = async (req, reply) => {
+  public delete = async (req, reply) : Promise<Object|Error> => {
     try {
       const { id } = req.params
-      return this.model.findByIdAndDelete(id)
+      return this.model.findByIdAndDelete(id).lean(true)
     } catch (e) {
       throw boom.boomify(e)
     }
