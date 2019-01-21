240978c336c5e3c6a6594dd0382eecf9a5afe7c4
Romain KELIFA <romain.kelifa@gmail.com>
2019-01-22 02:48:44 +0700

    :bulb: Update comments

--- src/loader.ts
+++ src/loader.ts
@@ -2,10 +2,11 @@ import * as fs from 'fs'
 import * as path from 'path'
 // location of the collections folders
 const location = [ __dirname, 'db' ]
-// will automatically load all the seeds from the collections folders
+// retrieve all the names of the folders
 export const collections = fs
 .readdirSync(path.resolve(...location))
 .filter(file => fs.lstatSync(path.resolve(...location.concat(file))).isDirectory())
+// will automatically load all the seeds from the collections folders
 export const seeds = collections.map(folder => ({
   name: folder, documents: require(path.resolve(...location, folder)).seeds,
 }))
