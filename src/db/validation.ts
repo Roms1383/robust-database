6cea9ba3c91127ba9895bf967bccb8a093db7533
Romain KELIFA <romain.kelifa@gmail.com>
2019-01-21 23:19:43 +0700

    :pencil2: Fix typo

--- src/db/validation.ts
+++ src/db/validation.ts
@@ -8,6 +8,9 @@ export const _id = Joi
   return { ...error, message: `${error.context.label} is not a valid ObjectID` }
 })
 .description(`MongoDB ObjectID
+the 12-byte ObjectId value consists of:
+- a 4-byte value representing the seconds since the Unix epoch,
+- a 5-byte random value, and
 - a 3-byte counter, starting with a random value`)
 export const __v = Joi
 .number()
